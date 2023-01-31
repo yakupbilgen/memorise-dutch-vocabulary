8a9f8d421ba97f53c32acfa1f4e3f341340ad545
yakupbilgen <yakupbilgen92@gmail.com>
2023-01-31 13:39:17 +0100

    sparated tp constant variable

--- lib/constant/app_constant.dart
+++ lib/constant/app_constant.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/material.dart';
-
 class AppConstant {
   static const appBarTitle = "Memorise Dutch Vocabulary";
-  static const bgColor = Color(0xFFEEE7D4);
 
-  static const defaultPadding = 20.0;
-  static const defaultHeight = 20.0;
+  static const defaultPadding = 12.0;
+  static const defaultHeight = 12.0;
 }
