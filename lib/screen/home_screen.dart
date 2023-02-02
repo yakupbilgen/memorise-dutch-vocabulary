61dd5dff1ca9a6c60d0e3daa5e6b359b3aa3e1d8
yakupbilgen <yakupbilgen92@gmail.com>
2023-02-02 16:26:33 +0100

    loadJson data succed

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,7 +6,6 @@ import 'package:flutter/services.dart';
 import '../constant/app_constant_appbar.dart';
 import '../constant/app_constant_color.dart';
 import '../constant/app_constant_text.dart';
-import '../constant/app_contant_size.dart';
 
 class HomeScren extends StatefulWidget {
   const HomeScren({super.key});
@@ -16,61 +15,59 @@ class HomeScren extends StatefulWidget {
 }
 
 class _HomeScrenState extends State<HomeScren> {
-  late var catalogData;
+  List _myVocabulary = [];
 
-  Future<String> loadData() async {
-    var data = await rootBundle.loadString("assets/json/vocabulary.json");
+  Future<void> readJson() async {
+    var response = await rootBundle.loadString('assets/json/vocabulary.json');
+    final data = await json.decode(response);
+
     setState(() {
-      catalogData = json.decode(data);
+      _myVocabulary = data["vocabulary"];
     });
-    return "success";
   }
 
   @override
   void initState() {
     super.initState();
-    loadData();
+    readJson();
   }
 
   @override
   Widget build(BuildContext context) {
-    final Size deviceSize = MediaQuery.of(context).size;
-
-    var futureBuilder = FutureBuilder(
-        future: loadData(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.connectionState == ConnectionState.done) {
-            return Center(
-                child: Text(catalogData.toString(), style: const TextStyle()));
-          } else {
-            return Container();
-          }
-        });
+    //final Size deviceSize = MediaQuery.of(context).size;
 
     return Scaffold(
       appBar: appAppBar(context, AppConstantText.appBarTitle, false, true),
       backgroundColor: AppConstantColor.bgColor,
-      body: Container(
-        padding: const EdgeInsets.only(
-            left: AppConstantSize.defaultPadding,
-            right: AppConstantSize.defaultPadding),
-        child: Center(
-          child: Text(
-              'witdh => ${deviceSize.width}\nheight => ${deviceSize.height}',
-              style: Theme.of(context)
-                  .textTheme
-                  .headline4
-                  ?.copyWith(color: Colors.orange)),
-        ),
-      ),
-      // body: Center(
-      //   child: Text(catalogData.toString()),
-      // ),
+      body: _myVocabulary.isNotEmpty
+          ? ListView.builder(
+              itemCount: _myVocabulary.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  key: ValueKey(_myVocabulary[index]['id']),
+                  margin: const EdgeInsets.all(12.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: AppConstantColor.iconColor,
+                      child: Text(_myVocabulary[index]["id"].toString(),
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6
+                              ?.copyWith(fontWeight: FontWeight.bold)),
+                    ),
+                    title: Text(_myVocabulary[index]["dutch"]),
+                    subtitle: Text(_myVocabulary[index]["turkish"]),
+                  ),
+                );
+              })
+          : const Center(
+              child: Text("Loading data"),
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          debugPrint(catalogData.toString());
+          debugPrint("click success");
+          debugPrint('${_myVocabulary.length}');
+          debugPrint("click success");
         },
         backgroundColor: Colors.orange,
         child: const Icon(
